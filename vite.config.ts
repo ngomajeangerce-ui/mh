68820777fc8d88435a5f418b999562618042dd88
ngomajeangerce-ui <ngomajeangerce@gmail.com>
2026-06-28 17:29:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,20 +17,19 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/mh/',   // AJOUTE CETTE LIGNE
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
